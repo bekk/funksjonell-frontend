4ac5ad9331b1c41af6d89575ce167cd43ee8b097
Ingar Almklov <ingara@gmail.com>
2016-03-28 22:55:23 +0200

    Remove redux-thunk

--- day3/src/configureStore.js
+++ day3/src/configureStore.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 import DevTools from './containers/DevTools';
 
 const storeEnhancer = compose(
-  applyMiddleware(thunk, createLogger()),
+  applyMiddleware(createLogger()),
   DevTools.instrument()
 );
 
